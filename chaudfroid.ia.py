03e295723656ae433c3eb88088e839a26984f3a0
mainkleouf <35737184+mainkleouf@users.noreply.github.com>
2019-09-13 22:17:12 +0200

    Update chaudfroid.ia.py

--- chaudfroid.ia.py
+++ chaudfroid.ia.py
@@ -4,18 +4,22 @@ from time import *
 nombre = 70
 nombreJ = 0
 nombreB = 0
-ma bite = 9
+neuf = 9
+GGJ = 0
+GGB = 0
 
 print("Jouer avec un bot")
-while ma bite != 0:
+while neuf != 0:
     nombreJ = int(input("Joueur 1 : "))
     nombreB = int(input("BOT : "))
     if nombreJ == 70:
-        ma bite = 0    
-    if nombreB == 70 or "":
-        ma bite = 0
-print("GG")
+        neuf = 0
+        GGJ = GGJ + 1
+    elif nombreB == 70:
+        neuf = 0
+        GGB = GGB + 1
 
-
-
-while nombreJ, nombreB != nombre:
+if GGJ == 1:
+    print("Bien joué ! Vous avez gagné !")
+elif GGB == 1:
+    print("Dommage.. retentez une prochaine fois !")
