613d51329d047e207631351795910c9a7813fd5a
mainkleouf <35737184+mainkleouf@users.noreply.github.com>
2019-09-13 21:53:22 +0200

    beta ia

---
+++ chaudfroid.ia.py
@@ -0,0 +1,12 @@
+from random import *
+from time import *
+
+nombre = 70
+nombreJ = 0
+nombreB = 0
+
+print("Jouer avec un bot")
+while nombreJ or nombreB != nombre:
+    nombreJ = int(input("Joueur 1 : "))
+    nombreB = int(input("BOT : "))
+print("GG")
