0ca7f68f460a2c62a27986f4e4fb857c5ffb9e75
mainkleouf <35737184+mainkleouf@users.noreply.github.com>
2019-09-16 11:06:59 +0200

    Update chaudfroid.ia.py

--- chaudfroid.ia.py
+++ chaudfroid.ia.py
@@ -2,38 +2,51 @@ from random import *
 from time import *
 
 nombre = randint(1,101)
+nombreBot = randint(1,101)
+nom = 0
+joueur = 0
+coup = 0
+diff = 0
 nombreJ = 0
 nombreB = 0
 PVJ = 3
 PVB = 3
 gg = 0
 
-print("Jouer avec un bot")
+print()
+print("Vous allez jouer contre un bot, vous devez trouver le nombre (entre 1 et 100).")
+print("Chaque nombres trouvés enlève 1 point de vie à l'adversaire.")
+print("Vous avez trois vies, bonne chance !")
 while gg != 1:
-    
+
     nombreB = randint(1,101)
-    nombreJ = int(input("Joueur 1 : "))
-    
+    nombreJ = int(input("Un nombre ? "))
+
     if nombreJ < nombre:
         print("C'est plus !")
+        print()
     elif nombreJ > nombre: 
         print("C'est moins !")
-        
+        print()
+
     if nombreJ == nombre:
         PVB = PVB - 1
         print("Vous avez infliger un de dégât à l'adversaire, il est à ", PVB)
+        print()
         nombre = randint(1,101)
-    elif nombreB == nombre:
+    elif nombreB == nombreBot:
         PVJ = PVJ - 1
         print("Vous avez perdu un point de vie, vous êtes à ", PVJ)
-        nombre = randint(1,101)
+        print()
+        nombreBot = randint(1,101)
 
     if PVJ == 0:
         gg = 1
     elif PVB == 0:
         gg = 1
-        
+
 if PVB == 0:
     print("Bien joué ! Vous avez gagné !")
 elif PVJ == 0:
     print("Dommage.. retentez une prochaine fois !")
+
